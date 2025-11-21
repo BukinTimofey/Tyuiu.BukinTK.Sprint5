882ebb2873714354f7c9562847e010c2cfaf9167
forengeit345 <timophey2907@gmail.com>
2025-11-21 16:01:07 +0500

    Add project files.

---
+++ Tyuiu.BukinTK.Sprint5.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BukinTK.Sprint5.Task0.V17.Lib
+
+{
+    public class DataService : ISprint5Task0V17
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double result = Math.Round(2.4 * Math.Pow(x, 2) + 0.4 * Math.Pow(x, 2) - 14 * x + 41, 3);
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            File.WriteAllText(tempFilePath, result.ToString());
+            return tempFilePath;
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint5.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Tyuiu.BukinTK.Sprint5.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\timop\AppData\Local\Temp\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
