951e11cb9c5670e2219d49bd72b53b177fd95a04
forengeit345 <timophey2907@gmail.com>
2025-11-23 11:14:09 +0500

    Task5

---
+++ Tyuiu.BukinTK.Sprint5.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BukinTK.Sprint5.Task5.V26.Lib
+{
+    public class DataService : ISprint5Task5V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            double positive = 0;
+            double negative = 0;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string numberStr in numbers)
+                    {
+                        if (double.TryParse(numberStr, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                        {
+                            if (number >= 0)
+                            {
+                                positive += number;
+                            }
+                            else
+                            {
+                                negative += number;
+                            }
+                        }
+                    }
+                }
+            }
+            res = Math.Round(positive - negative, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint5.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Tyuiu.BukinTK.Sprint5.Task5.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V26.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
