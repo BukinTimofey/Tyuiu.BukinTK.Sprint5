5028affe21f95355e668ae59cb0f8b0e938d36a1
forengeit345 <timophey2907@gmail.com>
2025-11-23 11:43:48 +0500

    Task6

---
+++ Tyuiu.BukinTK.Sprint5.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BukinTK.Sprint5.Task6.V26.Lib;
+
+namespace Tyuiu.BukinTK.Sprint5.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask5V26.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.BukinTK.Sprint5.Task6.V26/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BukinTK.Sprint5.Task6.V26.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Данные находятся в файле " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Итоговое число = " + res);
+        Console.ReadKey();
+    }
+}
